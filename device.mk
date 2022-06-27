SGCMarkus/android_device_lge_flashlmdd

flashlmdd: device device name
define device name so we can use it in sm8150-common to copy fstab/init files properly

device.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/lge/flashlmdd/flashlmdd-vendor.mk)
 
 DEVICE_PATH := device/lge/flashlmdd
+DEVICE_NAME := flashlmdd
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop

LGE-SM8250-AOSP/android_device_lge_timelm

device.mk
@@ -19,7 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lge/betalm/betalm-vendor.mk)
 
+
 DEVICE_PATH := device/lge/betalm
+
+DEVICE_NAME := betalm
+
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop